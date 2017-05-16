f776a9da37e92c4d853095425373888f20a15653
Kassis Maria Carina <kassiscarina@gmail.com>
2017-05-16 16:28:48 +0200

    version angular de la home.html

--- js/controllers/form.controller.js
+++ js/controllers/form.controller.js
@@ -1,9 +1,8 @@
 /*********************** Controller de la page de formulaire ****************/
 
-app.controller('FormCtrl',function($rootScope){
+app.controller('FormCtrl',function($rootScope) {
 
-	//Stockage du this
-	var form = this;
+    //Stockage du this
+    var form = this;
 
 });
-
--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -5,5 +5,48 @@ app.controller('HomeCtrl',function($rootScope){
 	//Stockage du this
 	var home = this;
 
-});
+    home.head ={
+			date:"Date",
+			name: "Titre",
+			montant: "Montant",
+			categorie: "Categorie"
+		};
 
+	home.body =[{
+			date: "10/05",
+			name: "Cinema Star Trek",
+			montant: "15",
+			categorie: "cinema"
+		},{
+			date: "08/05",
+			name: "Theatre La luciernaga",
+			montant: "10",
+			categorie: "Theatre"
+		},
+			{
+				date: "09/05",
+				name: "Parque Asterix",
+				montant: "55",
+				categorie: "Parque"
+			}
+		];
+
+    home.sort = {
+        column: 'montant',
+        descending: false
+    };
+
+    home.selectedCls = function(column) {
+        return column == home.sort.column && 'sort-' + home.sort.descending;
+    };
+
+    home.changeSorting = function(column) {
+        var sort = home.sort;
+        if (sort.column == column) {
+            sort.descending = !sort.descending;
+        } else {
+            sort.column = column;
+            sort.descending = false;
+        }
+ };
+})
