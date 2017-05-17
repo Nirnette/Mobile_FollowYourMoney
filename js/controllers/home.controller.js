667f0e71efcc4bbda6e98a1ecd332988522bccbe
Nirnette <lachieuse_pro@hotmail.com>
2017-05-17 14:33:20 +0200

    Add to homescreen

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -1,9 +1,10 @@
 /*********************** Controller de la home page ****************/
 
-app.controller('HomeCtrl',function($rootScope){
+app.controller('HomeCtrl',function($rootScope, NotificationFactory){
 
 	//Stockage du this
 	var home = this;
-
+	// NotificationFactory.sendNotification('un test à la con', 13);
+	// console.log('Je suis dans le controller')
 });
 
